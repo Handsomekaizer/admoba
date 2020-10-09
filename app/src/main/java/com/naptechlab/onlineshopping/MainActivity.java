e9703720ca97e8012319097ee0c3cd2ef01488e9
MD MONJUR MORSHED <morshed.naptechlabs@gmail.com>
2020-10-10 01:03:22 +0800

    first commit

---
+++ app/src/main/java/com/naptechlab/onlineshopping/MainActivity.java
@@ -0,0 +1,69 @@
+package com.naptechlab.onlineshopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        prepareAd();
+        ScheduledExecutorService scheduledExecutorService=
+                Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(mInterstitialAd.isLoaded()){
+                            mInterstitialAd.show();
+                        }
+                        else{
+                            Log.d("TAG","not loaded yet");
+                        }
+                         prepareAd();
+
+
+
+
+
+                    }
+
+                });
+            }
+        },10,10,TimeUnit.SECONDS);
+
+
+
+
+
+
+
+
+    }
+
+    private void prepareAd() {
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+    }
+
+
+}
